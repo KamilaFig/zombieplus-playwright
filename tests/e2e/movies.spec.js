a3c29fefaf6e8161c4b2e9261315247702cc7193
KamilaFig <kamilafig@gmail.com>
2025-04-08 10:56:10 -0300

    Adding an image to the registered movie

--- tests/e2e/movies.spec.js
+++ tests/e2e/movies.spec.js
@@ -4,12 +4,13 @@ const data = require('../support/fixtures/movies.json')
 const { executeSQL } = require('../support/database')
 
 test('Should register a new movie', async ({ page }) => {
+    await executeSQL(`DELETE FROM movies`)
+
     const movie = data.create
-    await executeSQL(`DELETE FROM movies WHERE title = '${movie.title}';`)
 
     await page.login.do('admin@zombieplus.com', 'pwd123','Admin')
-    await page.movies.create(movie.title, movie.overview, movie.company, movie.release_year)
-    await page.toast.containText('Cadastro realizado com sucesso!')
+    await page.movies.create(movie)
+    await page.toast.containText('UhullCadastro realizado com sucesso!')
 });
 
 test('Should not register movie when the required fields are empty', async ({ page }) => {
